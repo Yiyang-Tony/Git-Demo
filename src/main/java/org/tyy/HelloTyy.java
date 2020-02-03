c0aa04ec6dfdd65c3bd9ae7b1d2ae6920bf1ca91
tyy <Yiyang-Tony@qq.com>
2020-02-03 18:30:00 +0800

    update01

--- src/main/java/org/tyy/HelloTyy.java
+++ src/main/java/org/tyy/HelloTyy.java
@@ -1,5 +1,7 @@
 package org.tyy;
 
+import java.util.Scanner;
+
 /**
  * @ClassName HelloTyy
  * @Description TODO
@@ -8,7 +10,31 @@ package org.tyy;
  * @Version 1.0
  */
 public class HelloTyy {
+    /*
+     * @Author TongYiyang
+     * @Description //TODO
+     * @Date 18:29 2020/2/3
+     * @Param [args]
+     * @Return void
+     */
     public static void main(String[] args) {
-        System.out.println("Hello tongyiyang!");
+        Scanner sc = new Scanner(System.in);
+        int x,y;
+        int result;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        result = sum(x,y);
+        System.out.print("x + y = " + result);
+        sc.close();
+    }
+    /*
+     * @Author TongYiyang
+     * @Description //TODO
+     * @Date 18:29 2020/2/3
+     * @Param [a, b]
+     * @Return int
+     */
+    public static int sum(int a,int b){
+        return a+b;
     }
 }
