e6c0523d963a50781b08e3fc48d7bc6ad158900c
Yiyang-Tony <Yiyang-Tony@qq.com>
2020-02-03 18:04:15 +0800

    Initial commit

---
+++ src/main/java/org/tyy/HelloTyy.java
@@ -0,0 +1,14 @@
+package org.tyy;
+
+/**
+ * @ClassName HelloTyy
+ * @Description TODO
+ * @Author TongYiyang
+ * @Date 2020/2/3 18:02
+ * @Version 1.0
+ */
+public class HelloTyy {
+    public static void main(String[] args) {
+        System.out.println("Hello tongyiyang!");
+    }
+}
